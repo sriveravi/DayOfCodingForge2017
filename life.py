fb6b63324988966d81c955e86ed5972d10a88665
Samuel Rivera <sriveravi@gmail.com>
2017-11-18 14:09:27 -0500

    zombie and dead cases

--- life.py
+++ life.py
@@ -1,53 +1,112 @@
 import unittest
 import numpy as np
+import copy
 
-class BoardTest( unittest.TestCase):
+# class LifeGame():
+#     __init__(self):
+#         self.gameBoard =
+DEAD = 0
+ALIVE = 1
+ZOMBIE = 2
 
-    def test1Alive(self):
-        g = np.array( [[1, 0, 0],[ 0, 1, 0],[ 0, 0, 1]] )
-        x,y =  1,1
-        result = calcNewAlive( g, x,y)
-        self.assertEqual( result, 1)
+def getNeighbors( board,y,x):
+    neighbors = []
+    for y1 in range(-1,2):
+        for x1  in range(-1,2):
+            if x1 == 0 and y1 == 0:
+                pass
+            elif x + x1 < 0 or x + x1 >= board.shape[1]:
+                pass
+            elif y + y1 < 0 or y + y1 >= board.shape[0]:
+                pass
+            else:
+                neighbors.append(board[y+y1,x+x1])
+    return neighbors
 
-    def test2Alive( self):
-        g = np.array( [[1, 0, 0],[ 0, 0, 0],[ 0, 0, 1]] )
-        x,y =  1,1
-        result = calcNewAlive( g, x,y)
-        self.assertEqual( result, 0)
+def nextCellState(board,y,x):
+    currState = board[y,x]
+    neighbors = getNeighbors( board, y,x)
 
-    def test1Board(self):
-        g = np.array( [[0, 0, 0, 0],[ 0, 1,1, 0],[ 0, 1,1, 0],[ 0, 0, 0,0]] )
-        result = calcNewBoard( g)
-        self.assertTrue(np.array_equal(g, result))
+    # Zombie cases
+    if isZombie(currState):
+        if not hasLive( neighbors):
+            return DEAD
+        elif hasLive( neighbors):
+            return ZOMBIE
 
-    def test2Board(self):
-        g = np.array( [[0, 0, 0, 0, 0],[ 0, 0, 1,0, 0],[ 0, 0, 1,0, 0],[ 0, 0, 1,0, 0],[0, 0, 0, 0, 0]] )
-        gOut = np.array( [[0, 0, 0, 0, 0],[0, 0, 0, 0, 0], [0, 1, 1, 1, 0], [0, 0, 0, 0, 0],[0, 0, 0, 0, 0]])
-        result = calcNewBoard( g)
-        self.assertTrue(np.array_equal(gOut, result))
+    # Dead cases
+    if isDead( currState):
+        if zombiePlayground(neighbors):
+            return ZOMBIE
+        elif fertile(neighbors):
+            return ALIVE
 
-def calcNewAlive( board, x,y):
-    currentState = board[y,x]
-    subBoard = board[y-1:y+2, x-1:x+2]
-    neighborsAlive = np.sum( subBoard) - board[y,x]
+    return DEAD
 
-    if currentState == 0 and neighborsAlive == 3:
-        return 1
-    if currentState == 1 and (neighborsAlive == 2 or neighborsAlive ==3):
-        return 1
-    return 0
+def fertile( neighbors):
+    return len(liveNeighbors(neighbors)) == 3
 
-def calcNewBoard( board):
-    newBoard = np.zeros( board.shape)
-    for y in range(1,board.shape[0]):
-        for x in range(1,board.shape[1]):
-            newBoard[y,x] = calcNewAlive(board,x,y)
-
-    return newBoard #np.zeros(board.size)
+def zombiePlayground(neighbors):
+    return (len(liveNeighbors(neighbors))+ len(zombieNeighbors(neighbors)) )==4
 
 
+def liveNeighbors( neighborList):
+    return [val for val in neighborList if val == ALIVE  ]
+
+def zombieNeighbors( neighborList):
+    return [val for val in neighborList if val == ZOMBIE  ]
+
+def isZombie(state):
+    return state == ZOMBIE
+
+def isAlive( state):
+    return state == ALIVE
+
+def isDead( state):
+    return state == DEAD
 
 
+def hasLive( neighborList):
+    return len( liveNeighbors(neighborList)) >0
+
+
+class TestGrid(unittest.TestCase):
+
+    # def test_Something(self):
+    #     self.assertEqual('foo'.upper(), 'FOO')
+    #     self.assertTrue('FOO'.isupper())
+    #     self.assertFalse('Foo'.isupper())
+
+    def testZombieStarving(self):
+        g = np.zeros( (3,3))
+        g[1,1] = ZOMBIE
+        nextState = nextCellState(g, 1,1)
+        self.assertEqual( nextState, DEAD)
+
+    def testZombieFeeding(self):
+        g = np.zeros( (3,3))
+        g[1,1] = ZOMBIE
+        g[0,0] = ALIVE
+        nextState = nextCellState(g, 1,1)
+        self.assertEqual( nextState, ZOMBIE)
+
+    def testSpontaneousZombie( self):
+        g = np.array([[1,1,1],[1,0,0],[0,0,0]])
+        nextState = nextCellState(g, 1,1)
+        self.assertEqual( nextState, ZOMBIE)
+
+    def testBecomeAlive( self):
+        g = np.array([[1,1,1],[0,0,0],[0,0,0]])
+        nextState = nextCellState(g, 1,1)
+        self.assertEqual( nextState, ALIVE)
+
+    # def testGetNeighbors(self):
+    #     g = np.ones( (3,3))
+    #     n = [1,1,1, ])
 
 if __name__ == '__main__':
     unittest.main()
+
+# 0: dead
+# 1: alive
+# 2: zombie
